47c90fc0ed981599847f7c603b5f020b46862f88
Chris <christophe.barreto@gmail.com>
2014-04-02 10:13:11 +0200

    Begin parser Jackson

--- src/org/interfaces/IVirtualHost.java
+++ src/org/interfaces/IVirtualHost.java
@@ -7,7 +7,38 @@ package org.interfaces;
  * Time: 08:36
  * To change this template use File | Settings | File Templates.
  */
+
 public interface IVirtualHost
 {
+    //hosts
+    String[] getHosts();
 
+    //host
+    String getHost();
+    void setHost(String host);
+
+    //lb
+    String getRepartitionMethod();
+    void setRepartitionMethod();
+
+    //workers
+    String[] getWorker();
+
+    //headers
+    String[] getHeaders();
+
+    //incoming
+    String getIncoming();
+    void setIncoming(String incoming);
+
+    //Add (incoming)
+    String getAddIncoming();
+    void setAddIncoming(String add);
+
+    //outcoming
+    String getOutcoming();
+    void setOutcoming(String outcoming);
+
+    String getOutcomingAdd();
+    void setOutcomingAdd(String add);
 }
